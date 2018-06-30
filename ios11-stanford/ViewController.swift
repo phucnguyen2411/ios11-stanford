44d1ab109b0cfba75edfc1d947bff6daeb6bd4b1
Phuc .Ng <phucnguyen.2411@gmail.com>
2018-07-01 00:50:14 +0700

    [IOS]- This is first session

--- ios11-stanford/ViewController.swift
+++ ios11-stanford/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flip: \(flipCount)"
+        }
+    }// no need choose type for this, becasue 0 clearly Int
+    
+    var emojChooses: Array<String> = ["😝","😁","😝","😁"] // No need choose exactly type of variable
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton){
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoj: emojChooses[cardNumber], on: sender)
+            print("this is number of card: \(cardNumber)")
+        }else{
+            print("Error")
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func flipCard(withEmoj emoj: String, on button: UIButton){
+        print("this is a test\(emoj)")
+        if(button.currentTitle == emoj){
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        } else {
+            button.setTitle(emoj, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        
     }
-
-
+    
 }
 
