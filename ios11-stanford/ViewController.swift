4ea6ccd47762be4cf2ce0ea9c495671ec05ce399
Phuc .Ng <phucnguyen.2411@gmail.com>
2018-06-30 23:41:41 +0700

    Initial Commit

---
+++ ios11-stanford/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios11-stanford
+//
+//  Created by Phuc Nguyen on 6/30/18.
+//  Copyright © 2018 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
